508d63c39eff339ca15a2f82453a31e9d862e3ce
joongeo <100403704+joongeo@users.noreply.github.com>
2022-05-17 15:24:21 +0900

    delete
    delete useless files

--- Case2.cpp
+++
@@ -1,9 +0,0 @@
-#include "functions.h"
-
-
-using namespace std;
-
-int main()
-{
-	Couplefinder();;
-}
--- case3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "헤더.h"
-
-using namespace std;
-
-int main() {
-	C3Readcsv use;
-	use.matcher();
-}
